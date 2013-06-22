d40b9f2bd80fd05e3a1d7df26a57606c4823fd27
jean-michel guillaume <guillauj@gmail.com>
2013-06-22 22:30:50 +0200

    user management in templates

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,27 +2,48 @@ package controllers;
 
 import jsmessages.JsMessages;
 import models.Brand;
+import play.libs.F.None;
+import play.libs.F.Option;
+import play.libs.F.Some;
 import play.mvc.Controller;
 import play.mvc.Result;
+import securesocial.core.Identity;
 import securesocial.core.java.SecureSocial;
 
 public class Application extends Controller {
 
-	@SecureSocial.SecuredAction
 	public static Result brands() {
-		return ok(views.html.main.render("brands.title"));
+		return ok(views.html.main.render("brands.title", new None<Identity>()));
 	}
 
 	public static Result brandFromName(String name) {
 		Brand result = Brand.findByName(name);
 		if (result != null) {
-			return ok(views.html.main.render("brand.title"));
+			return ok(views.html.main.render("brand.title",
+					new None<Identity>()));
 		}
 		return notFound("No such brand found: " + name);
 	}
 
+	@SecureSocial.UserAwareAction
 	public static Result welcome() {
-		return ok(views.html.main.render("dimlib.welcome.title"));
+		return ok(views.html.main
+				.render("dimlib.welcome.title", getUserInCTX()));
+	}
+
+	private static Option<Identity> getUserInCTX() {
+		Object user = ctx().args.get(SecureSocial.USER_KEY);
+		Option<Identity> param = null;
+		if (user != null) {
+			if (Identity.class.isAssignableFrom(user.getClass())) {
+				param = new Some<Identity>((Identity) user);
+			} else if (Option.class.isAssignableFrom(user.getClass())) {
+				param = (Option<Identity>) user;
+			}
+		} else {
+			param = new None<Identity>();
+		}
+		return param;
 	}
 
 	public static Result jsMessages() {
