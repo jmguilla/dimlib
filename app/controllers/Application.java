7b427f5fe6c2f4bc3e5d8c40ae5bc81250795c9e
Jean-Michel Guillaume <guillauj@gmail.com>
2013-07-02 11:30:05 +0200

    format - cleanup

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,5 @@
 package controllers;
 
-import jsmessages.JsMessages;
 import models.Brand;
 import play.api.templates.Html;
 import play.mvc.Controller;
@@ -12,73 +11,75 @@ import securesocial.core.java.SecureSocial;
 
 public class Application extends Controller {
 
-	private static final Html defaultContent = Html
-			.apply("<div ng-view></div>");
+  private static final Html defaultContent = Html
+      .apply("<div ng-view></div>");
 
-	public static Result brands() {
-		return ok(views.html.main.render("dimlib.brands.title",
-				scala.Option.<Identity> apply(null), defaultContent));
-	}
+  public static Result brands() {
+    return ok(views.html.main.render("dimlib.brands.title",
+        scala.Option.<Identity> apply(null), defaultContent));
+  }
 
-	public static Result brandFromName(String name) {
-		Brand result = Brand.findByName(name);
-		if (result != null) {
-			return ok(views.html.main.render("dimlib.brand.title",
-					scala.Option.<Identity> apply(null), defaultContent));
-		}
-		return notFound("No such brand found: " + name);
-	}
+  public static Result brandFromName(String name) {
+    Brand result = Brand.findByName(name);
+    if (result != null) {
+      return ok(views.html.main.render("dimlib.brand.title",
+          scala.Option.<Identity> apply(null), defaultContent));
+    }
+    return notFound("No such brand found: " + name);
+  }
 
-	@SecureSocial.UserAwareAction
-	public static Result welcome() {
-		return ok(views.html.main.render("dimlib.welcome.title",
-				getUserInCTX(), defaultContent));
-	}
+  @SecureSocial.UserAwareAction
+  public static Result welcome() {
+    return ok(views.html.main.render("dimlib.welcome.title",
+        getUserInCTX(), defaultContent));
+  }
 
-	@SecureSocial.SecuredAction
-	public static Result submit() {
-		return ok();
-	}
+  @SecureSocial.SecuredAction
+  public static Result submit() {
+    return ok();
+  }
 
-	static Option<Identity> getUserInCTX() {
-		Object user = ctx().args.get(SecureSocial.USER_KEY);
-		Option<Identity> param = null;
-		if (user != null) {
-			if (Identity.class.isAssignableFrom(user.getClass())) {
-				param = new Some<Identity>((Identity) user);
-			} else if (Option.class.isAssignableFrom(user.getClass())) {
-				param = (Option<Identity>) user;
-			}
-		} else {
-			param = scala.Option.<Identity> apply(null);
-		}
-		return param;
-	}
+  static Option<Identity> getUserInCTX() {
+    Object user = ctx().args.get(SecureSocial.USER_KEY);
+    Option<Identity> param = null;
+    if (user != null) {
+      if (Identity.class.isAssignableFrom(user.getClass())) {
+        param = new Some<Identity>((Identity)user);
+      }
+      else if (Option.class.isAssignableFrom(user.getClass())) {
+        param = (Option<Identity>)user;
+      }
+    }
+    else {
+      param = scala.Option.<Identity> apply(null);
+    }
+    return param;
+  }
 
-	@SecureSocial.SecuredAction
-	public static Result contribute() {
-		return ok(views.html.main.render("dimlib.contribute.title",
-				getUserInCTX(), defaultContent));
-	}
+  @SecureSocial.SecuredAction
+  public static Result contribute() {
+    return ok(views.html.main.render("dimlib.contribute.title",
+        getUserInCTX(), defaultContent));
+  }
 
-	public static Result jsMessages() {
-		return ok(JsMessages.generate(null)).as("application/javascript");
-	}
+  public static Result jsMessages() {
+    return ok(JsMessages.generate(null)).as("application/javascript");
+  }
 
-	@SecureSocial.SecuredAction
-	public static Result account() {
-		return ok();
-	}
+  @SecureSocial.SecuredAction
+  public static Result account() {
+    return ok();
+  }
 
-	/**
-	 * Below that, related to partials angularjs
-	 */
-	public static Result partialsWelcome() {
-		return ok(views.html.welcome.render());
-	}
+  /**
+   * Below that, related to partials angularjs
+   */
+  public static Result partialsWelcome() {
+    return ok(views.html.welcome.render());
+  }
 
-	// @SecureSocial.SecuredAction TODO
-	public static Result partialsContribute() {
-		return ok(views.html.contribute.render());
-	}
+  // @SecureSocial.SecuredAction TODO
+  public static Result partialsContribute() {
+    return ok(views.html.contribute.render());
+  }
 }
