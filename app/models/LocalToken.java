17018dfc5371434f599afb979c13616501d99b89
Jean-Michel Guillaume <jean-michel.guillaume@amadeus.com>
2013-07-16 14:29:01 +0200

    format & cleanup

--- app/models/LocalToken.java
+++ app/models/LocalToken.java
@@ -14,37 +14,37 @@ import play.db.ebean.Model;
 
 @Entity
 public class LocalToken extends Model {
-	@Id
-	@Required
-	public String uuid;
+  @Id
+  @Required
+  public String uuid;
 
-	public Date creationTime, expirationTime;
+  public Date creationTime, expirationTime;
 
-	public boolean signUp, expired;
+  public boolean signUp, expired;
 
-	public String email;
+  public String email;
 
-	public static Finder<String, LocalToken> find = new Finder<String, LocalToken>(
-			String.class, LocalToken.class);
+  public static Finder<String, LocalToken> find = new Finder<String, LocalToken>(
+      String.class, LocalToken.class);
 
-	public LocalToken(securesocial.core.java.Token token) throws ParseException {
-		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		creationTime = df.parse(token.creationTime
-				.toString("yyyy-MM-dd HH:mm:ss"));
-		expirationTime = df.parse(token.expirationTime
-				.toString("yyyy-MM-dd HH:mm:ss"));
-		email = token.email;
-		uuid = token.uuid;
-		signUp = token.isSignUp;
-	}
+  public LocalToken(securesocial.core.java.Token token) throws ParseException {
+    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    creationTime = df.parse(token.creationTime
+        .toString("yyyy-MM-dd HH:mm:ss"));
+    expirationTime = df.parse(token.expirationTime
+        .toString("yyyy-MM-dd HH:mm:ss"));
+    email = token.email;
+    uuid = token.uuid;
+    signUp = token.isSignUp;
+  }
 
-	public securesocial.core.java.Token toToken() {
-		securesocial.core.java.Token token = new securesocial.core.java.Token();
-		token.creationTime = new DateTime(creationTime);
-		token.expirationTime = new DateTime(expirationTime);
-		token.email = email;
-		token.uuid = uuid;
-		token.isSignUp = signUp;
-		return token;
-	}
+  public securesocial.core.java.Token toToken() {
+    securesocial.core.java.Token token = new securesocial.core.java.Token();
+    token.creationTime = new DateTime(creationTime);
+    token.expirationTime = new DateTime(expirationTime);
+    token.email = email;
+    token.uuid = uuid;
+    token.isSignUp = signUp;
+    return token;
+  }
 }
--- app/models/Request.java
+++ app/models/Request.java
@@ -14,46 +14,46 @@ import play.db.ebean.Model;
 @Entity
 public class Request extends Model {
 
-	@Id
-	private Long id;
+  @Id
+  private Long id;
 
-	@Required
-	@ManyToOne
-	@JsonIgnore
-	private User user;
+  @Required
+  @ManyToOne
+  @JsonIgnore
+  private User user;
 
-	@ManyToOne
-	@Required
-	private Item item;
+  @ManyToOne
+  @Required
+  private Item item;
 
-	public static Finder<Long, Request> find = new Finder<Long, Request>(
-			Long.class, Request.class);
+  public static Finder<Long, Request> find = new Finder<Long, Request>(
+      Long.class, Request.class);
 
-	public static List<Request> fromUserEmail(String userEmail) {
-		return find.where().eq("user_email", userEmail).findList();
-	}
+  public static List<Request> fromUserEmail(String userEmail) {
+    return find.where().eq("user_email", userEmail).findList();
+  }
 
-	public Long getId() {
-		return id;
-	}
+  public Long getId() {
+    return id;
+  }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-	public User getUser() {
-		return user;
-	}
+  public User getUser() {
+    return user;
+  }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+  public void setUser(User user) {
+    this.user = user;
+  }
 
-	public Item getItem() {
-		return item;
-	}
+  public Item getItem() {
+    return item;
+  }
 
-	public void setItem(Item item) {
-		this.item = item;
-	}
+  public void setItem(Item item) {
+    this.item = item;
+  }
 }
